a6686b2d2e2461fac382c5224c0f5e3e6422b180
Jacinto Aisa Ibañez <jacinto@desaprendiendo.net>
2022-10-19 21:49:53 +0200

    envio

--- WebAplicationMonica/Models/DesignTimeJacintoContextFactory.cs
+++ WebAplicationMonica/Models/DesignTimeJacintoContextFactory.cs
@@ -14,8 +14,8 @@ namespace WebAplicationMonica.Models
             // If you use SQL Server Express, change connection string appropriately
             // NOTE: Do not hardcode connection strings in code.  
             // This is hard coded here to limit scope of demo.
-            //var connectionString = "Server=tcp:servidormicroservicios.database.windows.net,1433;Initial Catalog=WebApplicationMonica;Persist Security Info=False;User ID=jacinto;Password=P0t@toP0t@to;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-              var connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=University; Integrated Security=True;";
+            var connectionString = "Server=tcp:servidormonicasql.database.windows.net,1433;Initial Catalog=MonicaSQL;Persist Security Info=False;User ID=jacinto;Password=P0t@toP0t@to;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            //var connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=University; Integrated Security=True;";
 
             dbContextBuilder.UseSqlServer(connectionString);
             return new JacintoContext(dbContextBuilder.Options);
